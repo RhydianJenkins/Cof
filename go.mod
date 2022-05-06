e13cb0e0848cb4ae6ec782ea2717d040f19531b4
Rhydian Jenkins <rhydz@msn.com>
2022-05-06 16:16:57 +0100

    CLI tool added with basic wireframe

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/rhydianjenkins/cof
 
 go 1.18
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.6.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/urfave/cli/v2"
+)
+
+func init() {
+}
 
 func main() {
+	app := &cli.App{
+		Name:     "cof",
+		Version:  "v0.0.0",
+		Compiled: time.Now(),
+		Authors: []*cli.Author{
+			{
+				Name:  "Rhydian",
+				Email: "rhydz@msn.com",
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:    "add",
+				Aliases: []string{"a"},
+				Usage:   "Add a task to today",
+				Action: func(c *cli.Context) error {
+					fmt.Print("TODO")
+					return nil
+				},
+			},
+		},
+		Flags: []cli.Flag{},
+	}
 
+	app.Run(os.Args)
 }
