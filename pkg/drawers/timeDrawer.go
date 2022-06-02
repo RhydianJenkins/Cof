6b67d399a4878e6801a2d050e695938687101738
Rhydian Jenkins <rhydian.jenkins@wcbs.co.uk>
2022-06-02 13:01:41 +0100

    Refactor to use structs for drawers

--- pkg/drawers/timeDrawer.go
+++ pkg/drawers/timeDrawer.go
@@ -1,4 +1,4 @@
-package timeDrawer
+package drawers
 
 import (
 	"time"
@@ -7,8 +7,15 @@ import (
 	"github.com/rivo/tview"
 )
 
-func Draw(screen tcell.Screen, x int, y int, width int, height int) (int, int, int, int) {
+type TimeDrawer struct {
+}
+
+func (timeDrawer *TimeDrawer) Draw(screen tcell.Screen, x int, y int, width int, height int) (int, int, int, int) {
 	timeStr := time.Now().Format("Current time is 15:04:05")
 	tview.Print(screen, timeStr, x, height/2, width, tview.AlignCenter, tcell.ColorLime)
 	return 0, 0, 0, 0
+}
+
+func CreateTimeDrawer() *TimeDrawer {
+	return &TimeDrawer{}
 }
